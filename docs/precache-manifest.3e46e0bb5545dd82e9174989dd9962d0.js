5d371eaabb43a6dada9842e05687009efaa70fbb
Philippe De Pauw - Waterschoot <drdynscript@gmail.com>
2020-02-16 16:47:37 +0100

    Deploy to docs folder

---
+++ docs/precache-manifest.3e46e0bb5545dd82e9174989dd9962d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b64eef2ed6c5cc9a9d9852bf3d22b74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30b9cee669c3eab691b1",
+    "url": "/static/css/main.4a124a7c.chunk.css"
+  },
+  {
+    "revision": "b900b4f9a00d3594fb64",
+    "url": "/static/js/2.70c8b782.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/static/js/2.70c8b782.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30b9cee669c3eab691b1",
+    "url": "/static/js/main.43de927d.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
